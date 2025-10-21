8b7ad80897304122c904a9a5d61ab24ffb5243bd
waku <wakunguma13@gmail.com>
2025-10-21 12:22:52 -0600

    Add vercel and astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  vite: {
-    plugins: [tailwindcss(),sitemap()],
-  }
+    prefetch: true,
+    vite: {
+      plugins: [tailwindcss(),sitemap()],
+    },
+    adapter: !(process.env.VERCEL_ENV === "production" || process.env.VERCEL_ENV === "preview") ? node({
+      mode: "standalone"
+    }) : vercel()
 });
