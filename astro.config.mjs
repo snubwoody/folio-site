17ea45498230c9a3928f4cb4d0b1dac64b08c164
waku <wakunguma13@gmail.com>
2025-10-21 12:14:49 -0600

    Fetch download links using github api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(),sitemap()],
   }
 });
